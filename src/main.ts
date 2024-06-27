a41917564e33516b96cda1b209ec88613a6a506f
Rifki Rahadian <rrahadian94@gmail.com>
2024-06-27 17:24:16 +0700

    chore: increase upload limit

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  app.use(express.json({ limit: '10mb' }));
+  app.use(express.urlencoded({ limit: '10mb', extended: true }));
   await app.listen(3000);
 }
 bootstrap();
